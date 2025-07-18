bd463a963292ca4d517e4ba1d691b5dff828791a
Brennan <tanjialebrennan@gmail.com>
2025-07-19 00:28:42 +0800

    feat: add vercel analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,10 @@
 import { EnsureLocaleAndTranslateParam } from "@/components/EnsureLocaleAndTranslateParam";
+import { Analytics } from "@vercel/analytics/next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Geist, Geist_Mono } from "next/font/google";
 import { createTranslator } from "use-intl/core";
 import "../globals.css";
-
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -43,6 +43,7 @@ export default async function RootLayout({ children, params }: { children: React
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
+        <Analytics />
       </body>
     </html>
   );
